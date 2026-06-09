539bf5944fd753cd086c16868a52f77bf47354d0
lqur4 <2233073359@correo.ler.uam.mx>
2026-06-09 14:57:45 -0600

    Actualizacion 09/06/2026

--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
+let alfa = 255;
+let vel = 5;
+let destello = true;
+
 function setup() 
 {
-	createCanvas(400, 400);
+	createCanvas(1912, 948);
 }
 
 function draw()
 {
+    background(0)
+    fill(255, 255, 255)
+    circle(width / 2, height / 2, 150)
+
+    if(destello){
+        alfa+=vel;
+        if(alfa > 250 || alfa < 250)
+        {
+            
+        }
+    }
+
+    
 
 }
