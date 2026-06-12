0242a5f7928738e845608e72a01776abd45e0337
lqur4 <2233073359@correo.ler.uam.mx>
2026-06-12 13:10:17 -0600

    Actualizacion 12/06/2026

--- sketch.js
+++ sketch.js
@@ -1,26 +1,96 @@
-let alfa = 255;
-let vel = 5;
-let destello = true;
+const INTRO = 0;
+const FLASH = 1;
+const CALIBRACION = 2;
+let currentState = INTRO;
+let flashRadius = 0;
+let flashAlpha = 255;
+let flashActive = false;
 
-function setup() 
-{
-	createCanvas(1912, 948);
+function setup() {
+
+    createCanvas(windowWidth, windowHeight);
+    textAlign(CENTER, CENTER);
+    ellipseMode(CENTER);
 }
 
-function draw()
-{
-    background(0)
-    fill(255, 255, 255)
-    circle(width / 2, height / 2, 150)
+function draw() {
 
-    if(destello){
-        alfa+=vel;
-        if(alfa > 250 || alfa < 250)
-        {
-            
-        }
+    background(0);
+    switch (currentState) {
+        case INTRO:
+            drawIntro();
+            break;
+        case FLASH:
+            drawFlash();
+            break;
+        case CALIBRACION:
+            drawCalibration();
+            break;
+    }
+}
+
+function drawIntro() {
+    fill(255);
+    textSize(24);
+    text(
+        "Haz clic para comenzar",
+        width / 2,
+        height / 2
+    );
+}
+
+function drawFlash() {
+    noStroke();
+    fill(255, flashAlpha);
+    circle(
+        width / 2,
+        height / 2,
+        flashRadius
+    );
+    flashRadius += 80;
+    flashAlpha -= 4;
+    if (flashAlpha <= 0) {
+        currentState = CALIBRACION;
+    }
+}
+
+
+function drawCalibration() {
+    fill(255);
+    textSize(28);
+    text(
+        "Preparando calibración...",
+        width / 2,
+        height / 2 - 40
+    );
+    textSize(18);
+    fill(180);
+    text(
+        "Optimizando experiencia",
+        width / 2,
+        height / 2 + 10
+    );
+    text(
+        "Analizando dispositivo",
+        width / 2,
+        height / 2 + 40
+    );
+}
+
+function mousePressed() {
+    if (currentState === INTRO) {
+        currentState = FLASH;
+        flashRadius = 0;
+        flashAlpha = 255;
     }
 
-    
+}
+
+function windowResized() {
+
+    resizeCanvas(
+        windowWidth,
+        windowHeight
+    );
 
 }
