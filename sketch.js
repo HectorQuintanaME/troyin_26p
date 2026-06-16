475fceab14db14c913a451c6d69a8354c16a1440
lqur4 <2233073359@correo.ler.uam.mx>
2026-06-16 12:36:13 -0600

    Actualizacion 16/06/2026

--- sketch.js
+++ sketch.js
@@ -5,26 +5,15 @@ let currentState = INTRO;
 let flashRadius = 0;
 let flashAlpha = 255;
 let flashActive = false;
-let bodyPose;
-let video;
-let poses = [];
-let connections;
+
 
 function preload() {
   bodyPose = ml5.bodyPose();
 }
 
 function setup() {
-
     createCanvas(windowWidth, windowHeight);
     textAlign(CENTER, CENTER);
-    ellipseMode(CENTER);
-     
-    video = createCapture(VIDEO);
-    video.size(640, 480);
-    video.hide();
-    bodyPose.detectStart(video, gotPoses);
-    connections = bodyPose.getSkeleton();
 }
 
 function draw() {
@@ -42,37 +31,6 @@ function draw() {
             break;
     }
 
-    //ESQUELETO DE CONEXIONES
-
-    for (let i = 0; i < poses.length; i++) {
-    let pose = poses[i];
-    for (let j = 0; j < connections.length; j++) {
-      let pointAIndex = connections[j][0];
-      let pointBIndex = connections[j][1];
-      let pointA = pose.keypoints[pointAIndex];
-      let pointB = pose.keypoints[pointBIndex];
-      if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
-        stroke(255, 0, 0);
-        strokeWeight(2);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
-      }
-    }
-  }
-
-    //DIBUJAR CONEXIONES 
-
-  for (let i = 0; i < poses.length; i++) {
-    let pose = poses[i];
-    for (let j = 0; j < pose.keypoints.length; j++) {
-      let keypoint = pose.keypoints[j];
-      if (keypoint.confidence > 0.1) {
-        fill(0, 255, 0);
-        noStroke();
-        circle(keypoint.x, keypoint.y, 10);
-      }
-    }
-  }
-
 }
 function drawIntro() {
     fill(255);
@@ -89,6 +47,7 @@ function drawFlash() {
     if (flashAlpha <= 0) {
         currentState = CALIBRACION;
     }
+   
 }
 
 
@@ -113,8 +72,4 @@ function mousePressed() {
 
 function windowResized() {
     resizeCanvas(windowWidth,windowHeight);
-}
-
-function gotPoses(results) {
-    poses = results;
 }
